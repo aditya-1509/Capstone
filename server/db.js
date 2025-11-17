8e53ee0fe5172e9c3f5a1ef43f7f26d832dd59ce
aditya1000007 <adityarani.1509@gmail.com>
2025-11-17 13:26:26 +0530

    fixed mongo connection

--- server/db.js
+++ server/db.js
@@ -1,30 +1,13 @@
 const mongoose = require("mongoose");
-
-let isConnected = false;
+require('dotenv').config();
 
 const connectDB = async () => {
-  if (isConnected) {
-    // If already connected, just return
-    return;
-  }
-
-  if (!process.env.MONGODB_URI) {
-    throw new Error("MONGODB_URI is missing");
-  }
-
   try {
-    const db = await mongoose.connect(process.env.MONGODB_URI, {
-      dbName: "capstone", // optional: your DB name
-      bufferCommands: false, // prevents buffering errors
-      serverSelectionTimeoutMS: 10000, // prevents timeout error
-    });
-
-    isConnected = db.connections[0].readyState === 1;
-    console.log("MongoDB Connected:", isConnected);
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("MongoDB connected");
   } catch (error) {
     console.error("MongoDB connection failed:", error.message);
-    throw error;
   }
 };
 
-module.exports = connectDB;
+module.exports = connectDB
