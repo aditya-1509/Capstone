063e649a7a8c53702b6c47d60130f140e881cb72
aditya1000007 <adityarani.1509@gmail.com>
2025-11-17 16:32:09 +0530

    Added Logout

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
-
+import { redirect } from 'react-router-dom'
 const Dashboard = () => {
+  const navigate = useNavigation();
   return (
     <div>
       <h1 className="text-3xl font-bold underline">Dashboard</h1>
+      <button onClick={()=>{
+        localStorage.clear()
+        redirect('/login')
+      }}>Logout</button>
     </div>
   )
 }
